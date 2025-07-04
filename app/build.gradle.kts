3723d94a5c480c41bc2d2f857145cb79ed3edd3b
AhplaXx <necdet03fb@hotmail.com>
2025-07-04 10:59:16 +0300

    Timber added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,8 @@ dependencies {
     //Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-compiler:2.51.1")
-
+    //Timber
+    implementation ("com.jakewharton.timber:timber:5.0.1")
 
     // Hilt Navigation Compose
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
