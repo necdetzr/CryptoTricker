161c5ff438ab80d79708b93ade7a2c77673d7c04
AhplaXx <necdet03fb@hotmail.com>
2025-07-06 17:57:41 +0300

    Timber fixed to make compatible for crashlytics

--- app/src/main/java/com/necdetzr/loodoscrypto/utils/CrashlyticsTree.kt
+++ app/src/main/java/com/necdetzr/loodoscrypto/utils/CrashlyticsTree.kt
@@ -1,17 +1,27 @@
-package com.necdetzr.loodoscrypto.utils
-
 import android.util.Log
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 
-class CrashlyticsTree : Timber.Tree(){
+class CrashlyticsTree : Timber.Tree() {
+
     private val crashlytics = FirebaseCrashlytics.getInstance()
+
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        if(priority == Log.WARN || priority == Log.ERROR || priority == Log.DEBUG ){
-            crashlytics.log("$tag: $message")
-            t?.let { crashlytics.recordException(it) }
+        if (priority >= Log.ERROR) {
+            crashlytics.log(message)
+            if (t != null) {
+                crashlytics.recordException(t)
+            } else {
+                crashlytics.recordException(Throwable(message))
             }
-
-
+        } else {
+            when (priority) {
+                Log.VERBOSE -> Timber.v(message)
+                Log.DEBUG -> Timber.d(message)
+                Log.INFO -> Timber.i(message)
+                Log.WARN -> Timber.w(message)
+                else -> {}
+            }
+        }
     }
 }
