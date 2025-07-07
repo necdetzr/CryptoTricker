ec4cfdad237156f2742a1eb585d7557b9557487c
AhplaXx <necdet03fb@hotmail.com>
2025-07-07 11:22:37 +0300

    Remember Me Composable Component added.

---
+++ app/src/main/java/com/necdetzr/loodoscrypto/presentation/ui/main/components/RememberMe.kt
@@ -0,0 +1,30 @@
+package com.necdetzr.loodoscrypto.presentation.ui.main.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun RememberMe(checked:Boolean,onCheckedChange:(Boolean)-> Unit){
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            colors = CheckboxDefaults.colors(
+                checkedColor = MaterialTheme.colorScheme.primary
+            )
+        )
+        Text("Remember Me", style = MaterialTheme.typography.bodySmall, color = MaterialTheme.colorScheme.onBackground)
+
+    }
+
+}
