fa87c9c08627a786254deeaf06714c97d22c39ce
AhplaXx <necdet03fb@hotmail.com>
2025-07-16 10:10:16 +0300

    Util file added with OptionFunctions.

---
+++ feature/settings/src/main/java/com/example/settings/util/OptionsFunctions.kt
@@ -0,0 +1,29 @@
+package com.example.settings.util
+
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
+import java.util.Locale
+
+object OptionsFunctions {
+    fun setLocale(context: Context,languageCode:String): Context{
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+
+        val config = Configuration(context.resources.configuration)
+        config.setLocale(locale)
+        return context.createConfigurationContext(config)
+
+    }
+    fun restartAppWithLocale(activity: Activity, languageCode: String) {
+        setLocale(activity,languageCode)
+        val intent = activity.intent
+
+        activity.finish()
+        activity.overridePendingTransition(0,0)
+        activity.startActivity(intent)
+        activity.overridePendingTransition(0,0)
+
+    }
+}
