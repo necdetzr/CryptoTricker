5a46c9e575fcdaf311e004a249298efc3bf6901f
AhplaXx <necdet03fb@hotmail.com>
2025-07-17 13:06:53 +0300

    Android Application Plugin added.

---
+++ build-logic/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,72 @@
+package com.necdetzr.buildlogic
+
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.kotlin.dsl.withType
+
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.android.application")
+            pluginManager.apply("org.jetbrains.kotlin.android")
+
+            extensions.configure<ApplicationExtension> {
+                namespace = "com.necdetzr.loodoscrypto"
+
+                compileSdk = 35
+
+                defaultConfig {
+                    applicationId = "com.necdetzr.loodoscrypto"
+                    minSdk = 29
+                    targetSdk = 35
+                    versionCode = 1
+                    versionName = "1.0"
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+                    vectorDrawables {
+                        useSupportLibrary = true
+                    }
+                }
+
+                buildTypes {
+                    getByName("release") {
+                        isMinifyEnabled = false
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        )
+                    }
+                }
+
+                buildFeatures {
+                    compose = true
+                }
+
+                composeOptions {
+                    kotlinCompilerExtensionVersion = "1.6.10"
+                }
+
+                packaging {
+                    resources {
+                        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+                    }
+                }
+
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+
+            tasks.withType<KotlinCompile>().configureEach {
+                kotlinOptions {
+                    jvmTarget = "17"
+                }
+            }
+        }
+    }
+}
