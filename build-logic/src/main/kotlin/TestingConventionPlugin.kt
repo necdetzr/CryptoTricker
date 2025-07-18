02a979bcb0e83f8d639201f26e771bd6dd73b23f
AhplaXx <necdet03fb@hotmail.com>
2025-07-18 10:04:59 +0300

    Testing Convention Plugin added.

---
+++ build-logic/src/main/kotlin/TestingConventionPlugin.kt
@@ -0,0 +1,15 @@
+package com.necdetzr.buildlogic
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class TestingConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) = with(target) {
+        dependencies {
+            add("testImplementation", "junit:junit:4.13.2")
+            add("androidTestImplementation", "androidx.test.ext:junit:1.1.5")
+            add("androidTestImplementation", "androidx.test.espresso:espresso-core:3.5.1")
+        }
+    }
+}
