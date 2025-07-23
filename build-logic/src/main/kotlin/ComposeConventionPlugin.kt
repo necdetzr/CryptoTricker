17ba733ca05b8d830606b657990ccba45494125e
AhplaXx <necdet03fb@hotmail.com>
2025-07-23 10:21:48 +0300

    Compose Plugin fixed.

--- build-logic/src/main/kotlin/ComposeConventionPlugin.kt
+++ build-logic/src/main/kotlin/ComposeConventionPlugin.kt
@@ -1,12 +1,23 @@
-package com.necdetzr.buildlogic
 
+import com.android.build.api.dsl.ApplicationExtension
+import configureAndroidCompose
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.getByType
 
 class ComposeConventionPlugin : Plugin<Project> {
-    override fun apply(target: Project) = with(target) {
-        pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.android.application")
+            apply(plugin = "org.jetbrains.kotlin.plugin.compose")
 
-
+            afterEvaluate {
+                val extension = extensions.getByType<ApplicationExtension>()
+                configureAndroidCompose(extension)
+            }
+        }
     }
 }
+
+
