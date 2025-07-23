ebbea083fc30cf42fed109592968eac2ed3142ab
AhplaXx <necdet03fb@hotmail.com>
2025-07-23 10:33:31 +0300

    Core Gradles updated.

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -1,49 +1,21 @@
 plugins {
-    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.google.dagger.hilt.android")
-    kotlin("kapt")
-
+    alias(libs.plugins.necdet.android.library)
+    alias(libs.plugins.necdet.android.hilt)
+    alias(libs.plugins.necdet.android.library.compose)
 }
 
 android {
     namespace = "com.necdetzr.datastore"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    api(libs.androidx.dataStore.preferences)
     implementation(project(":analytics"))
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-compiler:2.51.1")
+    implementation(project(":core:common"))
+    api(libs.hilt.android.compiler)
+    api(libs.hilt.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
 }
--- core/monitor/build.gradle.kts
+++ core/monitor/build.gradle.kts
@@ -1,43 +1,18 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.necdet.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.necdet.android.hilt)
+    alias(libs.plugins.necdet.android.library.compose)
+
 }
 
 android {
-    namespace = "com.example.monitor"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+    namespace = "com.necdetzr.monitor"
 }
 
 dependencies {
-
+    implementation(project( ":core:common"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
 }
