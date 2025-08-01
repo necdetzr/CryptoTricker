7f67a583006ca53cdafd27003bb7c3dd98c075df
AhplaXx <necdet03fb@hotmail.com>
2025-08-01 10:49:14 +0300

    Network files turned to modular.

---
+++ core/network/src/main/java/com/example/network/di/FirebaseModule.kt
@@ -0,0 +1,24 @@
+package com.example.network.di
+
+import com.example.network.manager.FirebaseAuthManager
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class FirebaseModule {
+
+    @Singleton
+    @Provides
+    fun providesFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+    @Singleton
+    @Provides
+    fun providesFirebaseAuthManager(auth:FirebaseAuth): FirebaseAuthManager {
+        return FirebaseAuthManager(auth)
+    }
+}
