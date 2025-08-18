d04d915b8c7d67bf07edf222e1ea7ef09bcde3df
AhplaXx <necdet03fb@hotmail.com>
2025-08-18 12:59:21 +0300

    Settings ui updated.

--- feature/settings/src/main/java/com/example/settings/ui/components/SettingsSection.kt
+++ feature/settings/src/main/java/com/example/settings/ui/components/SettingsSection.kt
@@ -1,6 +1,8 @@
 package com.example.settings.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
@@ -22,7 +25,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -46,13 +51,25 @@ fun SettingsSection(
             )
 
     ) {
-        Icon(
-            imageVector = leadingIcon,
-            contentDescription = "leadingIcon",
-            tint = MaterialTheme.colorScheme.onSurface
-        )
+        Box(
+            modifier = Modifier.size(36.dp).clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary.copy(0.1f)),
+
+            contentAlignment = Alignment.Center
+        ){
+
+            Icon(
+                imageVector = leadingIcon,
+                contentDescription = "leadingIcon",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(18.dp)
+            )
+        }
+
         Spacer(modifier = Modifier.width(12.dp))
-        Text(title, style = MaterialTheme.typography.bodyLarge)
+        Text(title, style = MaterialTheme.typography.bodyLarge.copy(
+            fontWeight = FontWeight.Medium
+        ))
         Spacer(Modifier.weight(1f))
         if (switch) {
             Switch(checked = checked, onCheckedChange = { onCheckedChange() })
